a4e9a4ffe02882ce076ceb9087e6c0861b5f03b0
creavill <63218370+creavill@users.noreply.github.com>
2022-11-07 19:50:44 -0800

    test2

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -8,7 +8,7 @@ function App() {
         
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>  Howdy Partners  sdasda   </p>
+        <p>  Howdy Partners    </p>
       </header>
     </div>
   );
